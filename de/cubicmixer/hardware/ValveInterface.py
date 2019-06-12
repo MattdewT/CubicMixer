60cd5af8eeb8efdd0587f7f8a9b9c66b3e5fc18e
MattdewT <tiroch.software@gmx.de>
2019-06-13 00:24:55 +0200

    commented valve interface

--- de/cubicmixer/hardware/ValveInterface.py
+++ de/cubicmixer/hardware/ValveInterface.py
@@ -2,12 +2,29 @@ import abc
 
 
 class ValveInterface:
+
+    '''
+    ValveInterface that helps the user to create own custom valves module. As long the custom valve inheritance the
+    ValveInterface, the ValveMaster can interact with it.
+    '''
+
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
     def open(self, volume):
+
+        '''
+        should activated the valve to dispense the specified volume
+        :param volume: volume to dispense by the valve
+        '''
+
         raise NotImplementedError
 
     @abc.abstractmethod
     def get_position(self):
+
+        '''
+        should return the digital position of the valve, it doesnt need to be the connected pin of the valve
+        '''
+
         raise NotImplementedError
